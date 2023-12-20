d5fb1c0f38b6cbb4ba6f5c7b4e22054b2ada2c08
led93 <xluigix93@gmail.com>
2023-12-20 23:06:57 +0900

    updated config to resolve nvim-ts-context-commentstring dependency that was missing in treesitter

--- lua/lt/plugins/init.lua
+++ lua/lt/plugins/init.lua
@@ -1,5 +1,6 @@
 return {
-  "nvim-lua/plenary.nvim",
-  "inkarkat/vim-ReplaceWithRegister",
-  "christoomey/vim-tmux-navigator",
+	"nvim-lua/plenary.nvim",
+	"inkarkat/vim-ReplaceWithRegister",
+	"christoomey/vim-tmux-navigator",
+	"JoosepAlviste/nvim-ts-context-commentstring",
 }
